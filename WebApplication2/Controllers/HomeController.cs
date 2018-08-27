6f3344c994b9f49c5ae855861d25a2decfd91e2c
SimonTor <simon.torka@online.de>
2018-08-27 16:15:06 +0200

    upload data

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new DateTimeWrapper();
+            model.Now = DateTime.Now;
+            return View(model);
+        }
+	}
+}
